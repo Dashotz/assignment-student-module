57334796cea8ff92dd019b8ac60692f96eafc5ce
Dashotz <d.shotz@yahoo.com>
2025-01-10 20:47:35 +0800

    update students

---
+++ src/students/students.service.ts
@@ -0,0 +1,37 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Student } from './entities/student.entity';
+import { UpdateStudentDto } from './dto/update-student.dto';
+
+@Injectable()
+export class StudentsService {
+  constructor(
+    @InjectRepository(Student)
+    private studentRepository: Repository<Student>,
+  ) {}
+
+  async getAllStudents(): Promise<Student[]> {
+    return await this.studentRepository.find();
+  }
+
+  async getStudentById(id: number): Promise<Student> {
+    const student = await this.studentRepository.findOne({ where: { id } });
+    if (!student) {
+      throw new NotFoundException(`Student with ID ${id} not found`);
+    }
+    return student;
+  }
+
+  async updateStudent(
+    id: number,
+    updateStudentDto: UpdateStudentDto,
+  ): Promise<Student> {
+    const student = await this.studentRepository.findOne({ where: { id } });
+    if (!student) {
+      throw new NotFoundException(`Student with ID ${id} not found`);
+    }
+    Object.assign(student, updateStudentDto);
+    return await this.studentRepository.save(student);
+  }
+} 
